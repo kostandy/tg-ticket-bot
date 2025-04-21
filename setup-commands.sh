6fa8c36ee05d8052b6a87d502cd19ba015e9ae05
Dmytro Andriushchenko <spaser15@outlook.com>
2025-04-21 14:31:07 +0300

    feat: add script to set up bot commands

---
+++ setup-commands.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -f .dev.vars ]; then
+  source .dev.vars
+fi
+
+if [ -z "$TELEGRAM_BOT_TOKEN" ]; then
+  echo "Error: TELEGRAM_BOT_TOKEN is not set"
+  exit 1
+fi
+
+curl -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/setMyCommands" \
+     -H "Content-Type: application/json" \
+     -d '{
+       "commands": [
+         {
+           "command": "start",
+           "description": "Показати всі вистави"
+         },
+         {
+           "command": "subscribe",
+           "description": "Підписатися на сповіщення про квитки. Використання: /subscribe <ID вистави>"
+         }
+       ]
+     }' 
